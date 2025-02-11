b04723ff2ef202289b7c680d291e2ebea9f86bd1
Olivier Maraval <olivier.maraval@edu.univ-fcomte.fr>
2025-02-11 15:38:49 +0100

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by maraval olivier on 11/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
